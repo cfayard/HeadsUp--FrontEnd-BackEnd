e112302f2df6a1041d4fd100a9dddfea6f40b6e9
Casey Fayard <fayardcasey@gmail.com>
2020-03-13 13:55:59 -0400

    adding build files

---
+++ client/budget-app/build/precache-manifest.6af042b174f5a17634e449b65e471cd1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6fdee9df0765db55294bbc71db9813e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1322c022834fa78d6f62",
+    "url": "/static/css/main.8b640e13.chunk.css"
+  },
+  {
+    "revision": "95499cef76fa5dd8b13a",
+    "url": "/static/js/2.ac38cca8.chunk.js"
+  },
+  {
+    "revision": "0f628f634ecff46d2ac81c95c5b4e1a1",
+    "url": "/static/js/2.ac38cca8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1322c022834fa78d6f62",
+    "url": "/static/js/main.a82944cb.chunk.js"
+  },
+  {
+    "revision": "757981370a9ff71f2863",
+    "url": "/static/js/runtime-main.a5fdb4cb.js"
+  },
+  {
+    "revision": "0ee0922d074023423f75195444b88a89",
+    "url": "/static/media/Blocks-1.3s-241px (1).0ee0922d.gif"
+  },
+  {
+    "revision": "6e1be608d3c01653efad5a492edc1569",
+    "url": "/static/media/credit_card.6e1be608.png"
+  }
+]);
